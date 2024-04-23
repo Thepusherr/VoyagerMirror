29e74ba506fce992f81142ca113ae3a16aa2edb9
Pusher <spunkspunkik@gmail.com>
2024-04-23 18:55:08 +0300

    .gitignore updated

--- vendor/bundle/ruby/2.6.0/gems/zero_push-2.5.1/spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/spec'
-require 'webmock/minitest'
-require 'zero_push'
-require 'pry'
-
-ENV['AUTH_TOKEN'] ||= 'test-token'
-
-WebMock.enable!
--- vendor/bundle/ruby/3.2.0/gems/bundler-leak-0.3.0/spec/audit_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'bundler/plumber'
-
-describe Bundler::Plumber do
-  it "should have a VERSION constant" do
-    expect(subject.const_get('VERSION')).not_to be_empty
-  end
-end
