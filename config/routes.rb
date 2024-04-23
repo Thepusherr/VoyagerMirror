a2cb18fa34a2fb4fda02f51dfd2af265c8cfc8ef
Pusher <spunkspunkik@gmail.com>
2024-04-23 18:50:22 +0300

    fix logout error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get "up" => "rails/health#show", :as => :rails_health_check
 
   root to: "dashboard#index"
