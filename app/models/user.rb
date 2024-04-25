4ab26bdfad0c509c34035653c90311462dc0f9af
Pusher <spunkspunkik@gmail.com>
2024-04-25 18:38:34 +0300

    added avatar for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
-  # has_one_attached :avatar
-  #
+  has_one_attached :avatar
+
   has_many :user_teams, dependent: :destroy
   has_many :user_roles, dependent: :destroy
   has_many :teams, through: :user_teams
